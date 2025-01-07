66253265aead196b991fa0fca8a16ab4eaa0c19d
Troels Ugilt Jensen <6103205+tuj@users.noreply.github.com>
2025-01-07 15:58:14 +0100

    3197: Added echo to script

--- restart.sh
+++ restart.sh
@@ -4,3 +4,5 @@ docker compose --env-file .env.docker.local -f docker-compose.server.yml pull
 docker compose --env-file .env.docker.local -f docker-compose.server.yml up --force-recreate --detach --remove-orphans
 
 docker compose --env-file .env.docker.local -f docker-compose.server.yml exec --user deploy api bin/console doctrine:migrations:migrate --no-interaction
+
+echo "NB! Remember to load templates if they have changed. See 'Deployment' in README.md for instructions."
